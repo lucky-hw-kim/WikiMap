8dd175758ae1dac26007cb4a4fc4f61e4a541feb
Lucky Kim <lucky.hw.kim@gmail.com>
2022-04-12 01:01:10 +0000

    tinkering with error page

--- server.js
+++ server.js
@@ -56,6 +56,18 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/profile", (req, res) => {
+  res.render("profile");
+});
+
+app.get("/maps", (req, res) => {
+  res.render("maps");
+});
+
+app.get("*", (req, res) => {
+  res.render("errorPage404");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
